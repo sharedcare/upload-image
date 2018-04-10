50468d3b189ad43a55e213c887acf0fb6d0ba313
sharedcare <sharedcare@hotmail.com>
2018-04-09 22:10:32 -0500

    Add comments

--- encrypt.js
+++ encrypt.js
@@ -1,64 +1,103 @@
+/**
+ *  @fileOverview Generates the signature for AWS s3 HTTP request
+ *
+ *  @author       Yizhen Chen
+ *
+ *  @requires     NPM:npm_module
+ */
+
 var crypto = require("crypto");
 
+/**
+ * Generates all required elements for http request
+ * @param {object} params - query parameters
+ * @param {object} config - global configuration
+ * @return {object} - JSON object contains all required elements
+ */
 function signature(params, config) {
-    return {
-        endpoint_url: "https://" + config.bucket  + ".s3.amazonaws.com.",
-        params: signParams(params, config)
-    }
-}
-
-function signParams(params, config) {
     var credential = amzCredential(config);
     var policy = updatePolicy(params, config, credential);
     var b64policy = new Buffer(JSON.stringify(policy, null, "\t")).toString("base64");
     return {
-        key: params.filename,
-        acl: 'public-read',
-        policy: b64policy,
-        "content-type": params.contentType,
-        "x-amz-algorithm": config.algorithm,
-        "x-amz-credential": credential,
-        "x-amz-date": config.date,
-        "x-amz-signature": signPolicy(b64policy, config)
-    }
+        endpoint_url: "https://" + config.bucket  + ".s3.amazonaws.com",
+        params: {
+            key: params.filename,
+            acl: 'public-read',
+            policy: b64policy,
+            "Content-Type": params["Content-Type"],
+            "x-amz-algorithm": config.algorithm,
+            "x-amz-credential": credential,
+            "x-amz-date": config.date,
+            "x-amz-signature": signPolicy(b64policy, config)
+        }
+    };
 }
 
-function updatePolicy(params, config, credential) {
+/**
+ * Generates s3 policy
+ * @param {object} params - query parameters
+ * @param {object} config - global configuration
+ * @param {string} amzCredential
+ * @return {{expiration: string, conditions: *[]}}
+ */
+function updatePolicy(params, config, amzCredential) {
     return {
         expiration: new Date((new Date).getTime() + (5 * 60 * 1000)).toISOString(),
         conditions: [
             { bucket: config.bucket },
             { key: params.filename },
             { acl: "public-read" },
-            ["starts-with", "$Content-Type", "image/"],
+            {"Content-Type": params["Content-Type"]},
             ["content-length-range", config.expectedMinSize, config.expectedMaxSize],
             {"x-amz-server-side-encryption": "AES256"},
             ["starts-with", "$x-amz-meta-tag", ""],
 
-            {"x-amz-credential": credential},
+            {"x-amz-credential": amzCredential},
             {"x-amz-algorithm": config.amzAlgorithm},
             {"x-amz-date": config.date}
         ]
     };
 }
 
+/**
+ * Generates amzCredential
+ * @param {object} config - global configuration
+ * @return {string} - amzCredential
+ */
 function amzCredential(config) {
     var dateString = config.date.split("T")[0];
-    return [config.accessKey, dateString, config.region, 's3/aws4_request'].join('/')
+    return [config.accessKey, dateString, config.region, 's3/aws4_request'].join('/');
 }
 
+/**
+ * hmac hash function using crypto library
+ * @param {string} key
+ * @param {string} string
+ * @return {void|*|Promise<any>} - hash value
+ */
 function hmac(key, string) {
-    return crypto.createHmac('sha256', key).update(string).digest('hex');
+    var hmac = crypto.createHmac('sha256', key);
+    hmac.end(string);
+    return hmac.read();
 }
 
+/**
+ * Generates signature based on AWS Signature Version 4
+ * @param {string} stringToSign - s3 policy in base64 encode
+ * @param {object} config - global configuration
+ * @returns {string} - signed signature
+ */
 function signPolicy(stringToSign, config) {
     var dateString = config.date.split("T")[0];
     var dateKey = hmac("AWS4" + config.secretKey, dateString);
     var dateRegionKey = hmac(dateKey, config.region);
     var dateRegionServiceKey = hmac(dateRegionKey, "s3");
     var signingKey = hmac(dateRegionServiceKey, "aws4_request");
-    return hmac(signingKey, stringToSign);
-
+    return hmac(signingKey, stringToSign).toString('hex');
 }
 
-module.exports = { signature: signature};
+/**
+ * signature functions required for http request
+ * @export Signature
+ */
+module.exports = { Signature: signature};
--- index.js
+++ index.js
@@ -1,34 +1,60 @@
+/**
+ *  @fileOverview Main node.js handler for AWS lambda function
+ *
+ *  @author       Yizhen Chen
+ *
+ *  @requires     NPM:npm_module
+ *  @requires     ./encrypt.js:signature
+ */
+
 'use strict';
 
 const uuid = require("uuid");
 const sign = require("./encrypt");
 
-
+/**
+ * amzDate used in signing signature,
+ * should be specified in the ISO8601 formatted string.
+ * @example 20130728T000000Z
+ * @type {string}
+ */
 const dateISOString = new Date().toISOString();
 const amzDate = getAmzDate(dateISOString);
 
+/**
+ * The global configuration
+ * @type {{accessKey: *, secretKey: *, bucket: *, region: *, expectedMinSize: number, expectedMaxSize: number, amzAlgorithm: string, successUrl: string, date: *, clientAccessKey: *}}
+ */
 const config = {
-    accessKey: process.env.S3_ACCESS_KEY,
-    secretKey: process.env.S3_SECRET_KEY,
-    bucket: process.env.S3_BUCKET,
-    region: process.env.S3_REGION,
-    expectedMinSize: 0,
-    expectedMaxSize: 15000000,
-    amzAlgorithm: "AWS4-HMAC-SHA256",
-    successUrl: "http://success.com",
-    date: amzDate,
-    clientAccessKey: process.env.CLIENT_ACCESS_KEY
+    accessKey: process.env.S3_ACCESS_KEY,   // Your AWS access key ID
+    secretKey: process.env.S3_SECRET_KEY,   // Your AWS secret access key ID
+    bucket: process.env.S3_BUCKET,          // Your AWS s3 target bucket
+    region: process.env.S3_REGION,          // Your AWS s3 region of target bucket
+    expectedMinSize: 0,                     // Minimum file size to upload
+    expectedMaxSize: 15000000,              // Maximum file size to upload
+    amzAlgorithm: "AWS4-HMAC-SHA256",       // amzAlgorithm, default is AWS4-HMAC-SHA256 for AWS Signature Version 4
+    successUrl: "http://success.com",       // Redirect Url when post request(upload to s3) succeed
+    date: amzDate,                          // amzDate defined above
+    clientAccessKey: process.env.CLIENT_ACCESS_KEY  // Your client access key
 };
 
+/**
+ * This is AWS Lambda main handler function.
+ * All parameters are passed through event.
+ */
 exports.handler = (event, context, callback) => {
+    // Get all url query parameters
     const params = event.queryStringParameters;
-    const requiredParams = ["Content-Type", "clientAccessKey", "fileExtension"];
+    // Specify which parameters are required
+    const requiredParams = ["Content-Type", "fileExtension"];
     var response;
 
+    // Return 400 if query is empty
     if (!params) {
         response = constResponse(400, 'Parameters are required', null);
         callback(null, response);
     } else {
+        // Return 400 if required parameters are not satisfied
         for (var i in requiredParams) {
             if (!params[requiredParams[i]]) {
                 response = constResponse(400, requiredParams[i] + " is required", null);
@@ -37,19 +63,42 @@ exports.handler = (event, context, callback) => {
         }
     }
 
+    // This code is used for client authentication
+    /*
     if (params.clientAccessKey !== config.clientAccessKey) {
         response = constResponse(403, "Client access is denied due to wrong access key", null);
         callback(null, response);
     }
+    */
 
-    params["filename"] = uuid.v1() + params.fileExtension;
-    const signedParams =  sign.signature(params, config);
+    // Generates unique name for uploading file
+    params["filename"] = uuid.v1() + "." + params.fileExtension;
+
+    /**
+     * This contains all parameters that AWS s3 POST requires
+     * includes signature
+     * @type {{endpoint_url, params}|*}
+     */
+    const signedParams =  sign.Signature(params, config);
 
     response = constResponse(200, "Request succeed", signedParams);
-
     callback(null, response);
 };
 
+/**
+ * This generates a valid response
+ * based on given status code, message and return parameters.
+ * @example
+ * var statusCode = 400,
+ *     message = "Bad request!",
+ *     params = null;
+ *
+ * var response = constResponse(statusCode, message, params);
+ * @param {number} statusCode - status code to return
+ * @param {string} message
+ * @param {object} params
+ * @returns {object} valid response
+ */
 function constResponse(statusCode, message, params) {
     const headers = { "Content-Type": "application/json" };
     return {
@@ -62,10 +111,15 @@ function constResponse(statusCode, message, params) {
     };
 }
 
+/**
+ * Generates the amzDate based on the ISO date string.
+ * @param {string} dateStr - ISO 8601 date string
+ * @returns {string|*} - AWS ISO8601 formatted date string
+ */
 function getAmzDate(dateStr) {
     var chars = [":","-"];
     for (var i=0;i<chars.length;i++) {
-        while (dateStr.indexOf(chars[i]) != -1) {
+        while (dateStr.indexOf(chars[i]) !== -1) {
             dateStr = dateStr.replace(chars[i],"");
         }
     }
